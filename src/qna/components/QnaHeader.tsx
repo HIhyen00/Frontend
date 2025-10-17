14712dcc776689b735f19490a3b5caebaa367e74
JUYOUNGLEE <duddud931215@gmail.com>
2025-10-17 18:52:43 +0900

    fix:{이주영] 제목 수정

--- src/qna/components/QnaHeader.tsx
+++ src/qna/components/QnaHeader.tsx
@@ -12,7 +12,7 @@ const QnaHeader: React.FC<Props> = ({ onCreated }) => {
     return (
         <>
             <div className="flex justify-between items-center pt-3 pb-3 px-5 rounded-lg bg-white sticky top-0 z-20">
-                <h1 className="text-2xl font-bold ml-2 text-gray-900">커뮤니티</h1>
+                <h1 className="text-2xl font-bold ml-2 text-gray-900">Q&A</h1>
                 <button
                     onClick={() => setOpenCreateModal(true)}
                     className="bg-purple-600 text-white px-4 py-2 rounded-full font-semibold hover:shadow-lg hover:shadow-purple-500/30 transition duration-300 flex items-center group"
